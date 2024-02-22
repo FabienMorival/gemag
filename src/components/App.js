23f2a80743f4348bb0cecf7bc47d0621bd04061b
Fabien Morival <fabienmorival@gmail.com>
2024-02-22 15:03:03 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import Nav from './nav/Nav';
+import './App.scss';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Nav/>
+    </div>
+  );
+}
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,8 @@
+import './Nav.scss';
+
+export default function Nav() {
+  return (
+    <nav className="nav">
+    </nav>
+  );
+}
