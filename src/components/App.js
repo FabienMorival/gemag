be67335ff918e9fff7e2d76d378518c85c9854a3
Fabien Morival <fabienmorival@gmail.com>
2024-02-29 16:29:38 +0100

    Ajout du routeur

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './nav/Nav';
 import Footer from './footer/Footer';
 import './App.scss';
 
-/**
- * App - Contient la globalité de l'application
- */
+import Home from './home/Home';
+import Astronomie from './article/astronomie/Astronomie';
+
 export default function App() {
   return (
     <div className="app">
-      <Nav/>
-      <Footer/>
+      <Router>
+        <Nav/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/astronomie" element={<Astronomie/>} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
