f62973850c61792bdcd34e151e8497fe9313de0b
Mikano576 <mikano57600@gmail.com>
2024-02-29 16:48:09 +0100

    Modif css + ajout photo

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,6 +6,7 @@ import './Home.scss';
 export default function Home() {
   return (
     <div className="home-page">
+      <img src="/image/photoGroupe.jpg" alt="Photo de groupe" className="photoGroupe"/>
     </div>
   );
 }
