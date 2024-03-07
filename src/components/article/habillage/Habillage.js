3550c1e14a3aa0ae02c354c1096c61ff11c6fc26
Mikano576 <mikano57600@gmail.com>
2024-03-07 16:37:39 +0100

    Article Habillage

--- src/components/article/habillage/Habillage.js
+++ src/components/article/habillage/Habillage.js
@@ -7,7 +7,22 @@ import Article from '../Article';
 export default function Habillage () {
   return (
     <Article article="habillage"> {/* Utilise l'architecture de base des articles */} 
-      {/* contenu */}
+      <div>
+        <h1 className="titreRubrique">Titre de la Rubrique</h1>
+      </div>
+      <div id='flex-container'>
+		    <div class='flex-items'>
+          <video controls>
+            <source src="./videos/Tomahawk2.mp4"></source>
+          </video>
+		    </div>
+        <div class='flex-items'>
+          <h1 className="titrePoste" >Titre du Poste</h1>
+          <div class='content'>
+            <p>Du texte plus long et avec des trucs en plus.</p>
+          </div>
+        </div>
+	    </div>
     </Article>
   );
 }
