c492988974f4e0c38a7defffdda3dcb6713a62a5
lukasleraillegema <lukas.leraille.edu@groupe-gema.com>
2024-03-14 14:58:15 +0100

    Bonjour
    IUOYAHEZBZFLIHVZEGVBVVIYLbEFG.KJHhzaaegFVLJKQZEBRGKLJ/BNAQRV.BQMLHIRVB

--- src/components/other/video/Video.js
+++ src/components/other/video/Video.js
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 import './Video.scss';
 
-export default function Video() {
+export default function Video(props) {
   return (
       
     <div className="video">
+      <h1>{props.title}</h1>
+      <video controls>
+        <source src={props.src}/>
+      </video>  
     </div>
   );
 }
