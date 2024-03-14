3a8468ecd3c1a7427116cb266d95f700a9aa3748
Fabien Morival <fabienmorival@gmail.com>
2024-03-14 16:15:13 +0100

    Correction des merge

--- src/components/article/habillage/Habillage.js
+++ src/components/article/habillage/Habillage.js
@@ -10,19 +10,48 @@ export default function Habillage () {
       <div>
         <h1 className="titreRubrique">Titre de la Rubrique</h1>
       </div>
+<br></br><br></br><br></br><br></br>
       <div id='flex-container'>
-		    <div class='flex-items'>
-          <video controls>
+        <div class='flex-items'>
+          <video class='videoMilieu' controls>
             <source src="./videos/Tomahawk2.mp4"></source>
           </video>
-		    </div>
+        </div>
         <div class='flex-items'>
           <h1 className="titrePoste" >Titre du Poste</h1>
           <div class='content'>
             <p>Du texte plus long et avec des trucs en plus.</p>
           </div>
         </div>
-	    </div>
+      </div>
+<br></br><br></br><br></br><br></br>
+      <div id='flex-container'>
+      <div class='flex-items'>
+          <h1 className="titrePoste" >Titre du Poste</h1>
+          <div class='content'>
+            <p>Du texte plus long et avec des trucs en plus.</p>
+          </div>
+        </div>
+        <div class='flex-items'>
+          <video class='videoMilieu' controls>
+            <source src="./videos/Tomahawk2.mp4"></source>
+          </video>
+        </div>
+      </div>
+<br></br><br></br><br></br><br></br>
+      <div id='flex-container'>
+        <div class='flex-items'>
+          <video class='videoMilieu' controls >
+            <source src="./videos/Tomahawk2.mp4"></source>
+          </video>
+        </div>
+        <div class='flex-items'>
+          <h1 className="titrePoste" >Titre du Poste</h1>
+          <div class='content'>
+            <p>Du texte plus long et avec des trucs en plus.</p>
+          </div>
+        </div>
+      </div>
     </Article>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,9 @@
 import { Link } from 'react-router-dom';
 import './Home.scss';
+import articles from '../articles.json';
 
 import Carousel from './Carousel';
 
-const articles = [
-  { name: "Associations", img: "associations.jpeg", link: "/associations" },
-  { name: "Astronomie", img: "astronomie.jpeg", link: "/astronomie" },
-  { name: "Sport", img: "sport.png", link: "/sport" },
-  { name: "Cuisine", img: "cuisine.jpg", link: "/cuisine" },
-  { name: "Habillage", img: "habillage.png", link: "/habillage" },
-  { name: "Déssalement", img: "dessalement.jpg", link: "/dessalement" }
-]
 
 /**
  * Page d'accueil du site
