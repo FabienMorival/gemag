fc207d499c02680cd44bdfa9c1f19c11a37b1603
lukasleraillegema <lukas.leraille.edu@groupe-gema.com>
2024-03-28 16:44:17 +0100

    sport
    début sport

--- src/components/article/dessalement/Dessalement.js
+++ src/components/article/dessalement/Dessalement.js
@@ -14,13 +14,13 @@ export default function Dessalement () {
   return (
     <Article article="dessalement"> {/* Utilise l'architecture de base des articles */} 
     <Title>De la Mer au Verre</Title>
-    <Paragraph title="Introduction">L’eau est un bien indispensable à la vie qui, malgré son abondance sur la terre, n’est pas accessible pour tous. En effet,  2,2 Milliards de personnes dans le monde n’ont pas accès à l’eau salubre. (Source: Unicef, 2024) Chercheurs, entrepreneurs et citoyens tentent de trouver des solutions pour dessaler l’eau de mer  de manière durable. Sur Terre, aujourd'hui 1% de l’eau potable consommée provient du dessalement de l’eau de mer ou d’eaux saumâtres  avec des procédés utilisant des énergies carbonées. 
+    <Paragraph centered title="Introduction">L’eau est un bien indispensable à la vie qui, malgré son abondance sur la terre, n’est pas accessible pour tous. En effet,  2,2 Milliards de personnes dans le monde n’ont pas accès à l’eau salubre. (Source: Unicef, 2024) Chercheurs, entrepreneurs et citoyens tentent de trouver des solutions pour dessaler l’eau de mer  de manière durable. Sur Terre, aujourd'hui 1% de l’eau potable consommée provient du dessalement de l’eau de mer ou d’eaux saumâtres  avec des procédés utilisant des énergies carbonées. 
 <br/><br/>A l’heure actuelle, les 2 principaux procédés sont : 
 <br/>Les procédés thermiques comme la distillation.
 <br/>Les procédés utilisant des membranes comme l'osmose inverse.
 </Paragraph>
     <div className='half-section'>
-    <Paragraph title="Distillation Thermique">Dans les procédés de distillation, il s'agit de chauffer l'eau de mer pour la vaporiser. La vapeur ainsi produite ne contient pas de sels, il suffit alors de condenser cette vapeur pour obtenir de l'eau douce liquide. Il s'agit en fait d'accélérer le cycle naturel de l'eau. (En effet l'eau s'évapore naturellement des océans, la vapeur s'accumule dans les nuages puis l'eau douce retombe sur terre par les précipitations.) 
+    <Paragraph centered title="Distillation Thermique">Dans les procédés de distillation, il s'agit de chauffer l'eau de mer pour la vaporiser. La vapeur ainsi produite ne contient pas de sels, il suffit alors de condenser cette vapeur pour obtenir de l'eau douce liquide. Il s'agit en fait d'accélérer le cycle naturel de l'eau. (En effet l'eau s'évapore naturellement des océans, la vapeur s'accumule dans les nuages puis l'eau douce retombe sur terre par les précipitations.) 
 <br/>Ce principe de dessalement très simple a été utilisé dès l'Antiquité pour produire de très faibles quantités d'eau douce sur les bateaux.
 <br/>Cependant, la distillation présente une consommation énergétique élevée en raison de la chaleur nécessaire pour vaporiser l'eau. La transformation d'un kg d'eau liquide en vapeur à 100 °C requiert environ 2250 kilojoules. Pour minimiser cette consommation, des procédés à multiples effets ont été développés, permettant de réutiliser l'énergie libérée lors de la condensation des vapeurs.
 <br/><br/>Deux procédés se partagent le marché du dessalement thermique : <Link to="/dessalementplus#pararaph-MSF">le procédé de distillation à détentes étagées (Multi-Stage Flash distillation MSF)</Link>
@@ -34,7 +34,7 @@ export default function Dessalement () {
       <Image src="/images/dessalement.jpg"/>
     </div>
     <div className='two-third-section'>
-    <Paragraph title="Osmose & Osmose inverse">
+    <Paragraph centered title="Osmose & Osmose inverse">
 <b><Link to="/dessalementplus#pararaph-Osmose">Osmose :</Link></b> 
 <br/>La pression osmotique est une force déterminée par une différence de concentration entre deux solutions situées de part et d'autre d'une membrane semi-perméable. 
 <br/>Une membrane semi-perméable est une membrane qui laisse passer le solvant ( l’eau) mais pas le soluté (sel). 
@@ -43,7 +43,7 @@ export default function Dessalement () {
 </Paragraph>
 </div>
 <br/>
-<Paragraph title="Conclusion" > 
+<Paragraph centered title="Conclusion" > 
   L'eau est un bien indispensable pour la survie humaine, mais la diminution des ressources d'eaux douces et la pollution des eaux de surfaces et souterraines ont un impact négatif sur l'accès à l'eau potable. Ainsi le dessalement de l'eau de mer s’avère être une solution, toutefois coûteuse, énergivore et aux impacts environnementaux négatifs avec ses rejets de saumure concentrée. Mais l'utilisation de sources d'énergies renouvelables et de technologies innovantes permettrait de minimiser les externalités négatives et de réduire les coûts. 
 <br/>Pour être une solution vraiment révolutionnaire, c'est-à-dire écologique, le dessalement d’eau de mer devra être associé à des procédés totalement innovants encore à l’état de projets  tels que  le MOF, The Pipe et Heliowater.
  </Paragraph>
--- src/components/article/sport/Sport.js
+++ src/components/article/sport/Sport.js
@@ -12,8 +12,21 @@ export default function Sport () {
   return (
     <Article article="sport"> 
       <Title>Le Sport</Title>
-      <Paragraph title="Lorem Ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Paragraph>
-      <Video src='./videos/Tomahawk2.mp4' title="bidule"/>
+      <div className='half-section'>
+      <Paragraph centered title="Mon Histoire">Je suis Gouly Kéone, né en 2000 à Bordeaux et passionné de foot.
+Après pas mal de déménagement, je suis arrivé avec ma famille en région parisienne.
+Jouant au foot partout ou je passais, des amateurs de football de mon quartier me remarquent et décident de m'inscrire dans le club de ma ville.
+Surclassé dans toutes les catégories de jeunes et performant, j'ai suscité l'intérêt de pas mal centre de pré-formation. 
+Suite à une longue période de réflexion de mes parents. On décide de rejoindre Montpellier.
+Lorsqu'on est enfant/ado et doué dans un sport, on ne se sent important uniquement au moment ou nous exerçons et n'avons pas la maturité de penser qu'il y d'autres choses plus importantes que notre sport. Et la plupart des gens autour de nous ne pensent qu'à leur intérêt avant tout.
+Seul ton niveau sera le critère dans leur prises de décision.
+Après pas mal de hauts et énormément de bas, j'ai quitté le club et décidé de rejoindre l'Angleterre après l'obtention de mon bac à l'âge de 18ans. J'ai évolué dans pas mal de club, j'ai essayé de commencer un cursus universitaire mais je n'ai malheureusement pas pu aller plus loin car mon engagement envers mon équipe et les moyens mis à disposition par l'école pour mon apprentissage ne me permettait pas d'étudier efficacement car le foot était ma priorité .
+Aujourd'hui, après un passage en Angleterre, Chypre, Grèce et l'Italie je me retrouve en incapacité d'exercer pour un certain moment.
+Toujours à la poursuite d'un avenir brillant et ne voulant pas rester à ne rien faire, j'ai décidé de commencer mon cursus universitaire en Bachelor 1.</Paragraph>
+</div>
+<div className='half-section'>
+<Video src='./videos/Tomahawk2.mp4' title="Video"/>
+</div>
     </Article>
     
   );
